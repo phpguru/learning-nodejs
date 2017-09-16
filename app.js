12d7bd2558b141b433836c97b700522ab8fcdb4d
yoyakem <tyler.price.g@gmail.com>
2017-09-16 11:36:09 -0700

    basic routing

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
    console.log('request was made: ' + req.url);
-   res.writeHead(200, {'Content-Type': 'application/json'});
-   var myObj = {
-   	name: 'Tyler',
-   	job: 'Pizza Slanger',
-   	age: 22
-   };
-   	res.end(JSON.stringify(myObj));
+   if(req.url === '/home' || req.url ==='/'){
+   	res.writeHead(200, {'Content-Type': 'text/html'});
+   	fs.createReadStream(__dirname + '/index.html').pipe(res);
+   } else if (req.url === '/contact'){
+   	res.writeHead(200, {'Content-Type': 'text/html'});
+   	fs.createReadStream(__dirname + '/contact.html').pipe(res);
+   } else if (req.url === '/api/ninjas'){
+   	var ninjas = [{name: 'tyler', age: 22}, {name: 'yoshi', age: 33}];
+   	res.writeHead(200, {'Content-Type': 'application/json'});
+   	res.end(JSON.stringify(ninjas));
+   } else {
+   		res.writeHead(404, {'Content-Type':'text/html'});
+   		fs.createReadStream(__dirname + '/404.html').pipe(res);
+	}
 
 });
 
