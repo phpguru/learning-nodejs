ad06753e6a2ce6d5df515d2a1f8ef3af8ee1d8f4
phpguru <geoffrey.hoffman@gmail.com>
2017-09-16 11:36:02 -0700

    Basic Routing

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
 	console.log('Request was made '+ req.url);
-	res.writeHead(200, {'Content-TYpe':'application/json'});
-	var myObj = {
-		name: 'Geoff',
-		job: 'Guru',
-		age: 46
-	};
-	res.end(JSON.stringify(myObj));
+	if (req.url === '/' || req.url === '/home') {
+		res.writeHead(200, {'Content-Type':'text/html'});
+		fs.createReadStream(__dirname + '/index.html').pipe(res);
+	} else if (req.url === '/contact') {
+		res.writeHead(200, {'Content-Type':'text/html'});
+		fs.createReadStream(__dirname + '/contact.html').pipe(res);
+	} else if (req.url === '/styles.css') {
+		res.writeHead(200, {'Content-Type':'text/css'});
+		fs.createReadStream(__dirname + '/styles.css').pipe(res);
+	} else if (req.url === '/api/gurus') {
+		res.writeHead(200, {'Content-Type':'application/json'});
+		var gurus = [
+		{name: 'Geoff', age: 46},
+		{name: 'Brandon', age: 33},
+		{name: 'Tyler', age: 22}
+		];
+		res.end(JSON.stringify(gurus));
+	} else {
+		res.writeHead(404, {'Content-Type':'text/html'});
+		fs.createReadStream(__dirname + '/404.html').pipe(res);
+	}
 });
 
 server.listen(3000, '127.0.0.1');
