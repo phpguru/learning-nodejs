d71f0aa4dcc20fe9d7571628e88ce9e204481e66
BrandoB <realbrandonb602@gmail.com>
2017-09-16 09:54:26 -0700

    Webserver Streaming to a Pipe

--- app.js
+++ app.js
@@ -1,25 +1,13 @@
 var http = require('http');
 var fs = require('fs');
 
-var myReadStream = fs.createReadStream (__dirname + '/random.txt');
-
-myReadStream.on('data', function(chunk){
-	console.log('new chunk received;');
-	console.log(chunk);
-})
-
-
-
-
-
-/*
 var server = http.createServer(function(req, res){
 	console.log("request was made "+req.url)
 res.writeHead(200, {'Content-Type': 'text/plain'});
-res.end('Hey ninjas');
+var myReadStream = fs.createReadStream (__dirname + '/random.txt', 'utf8');
+myReadStream.pipe(res);
 });
 
 
 server.listen(3000, '127.0.0.1');
 console.log('whatup, now listening to port 3000');
-*/
