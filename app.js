d5c25813855368b7194e60c26ffe308d4df3dd31
phpguru <geoffrey.hoffman@gmail.com>
2017-09-16 09:54:19 -0700

    Web server streaming to a pipe

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 var http = require('http');
 var fs = require('fs');
-var myReadStream = fs.createReadStream(__dirname + '/loremipsum.txt', 'utf8');
-var myWriteStream = fs.createWriteStream(__dirname + '/written.txt');
 
-myReadStream.pipe(myWriteStream);
-
-
-/*
 var server = http.createServer(function(req, res){
 	console.log('Request was made '+ req.url);
 	res.writeHead(200, {'Content-TYpe':'text/plain'});
-	res.end('Hey Ninjas!');
+	var myReadStream = fs.createReadStream(__dirname + '/loremipsum.txt', 'utf8');
+	myReadStream.pipe(res);
 });
 
 server.listen(3000, '127.0.0.1');
 console.log('Listening on port 3000');
-*/
