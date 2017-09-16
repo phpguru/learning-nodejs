5ae1bbc1b633889351377d8ecab90517b33c8774
phpguru <geoffrey.hoffman@gmail.com>
2017-09-16 10:48:01 -0700

    Creating an API

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ var fs = require('fs');
 
 var server = http.createServer(function(req, res){
 	console.log('Request was made '+ req.url);
-	res.writeHead(200, {'Content-TYpe':'text/html'});
-	var myReadStream = fs.createReadStream(__dirname + '/index.html', 'utf8');
-	myReadStream.pipe(res);
+	res.writeHead(200, {'Content-TYpe':'application/json'});
+	var myObj = {
+		name: 'Geoff',
+		job: 'Guru',
+		age: 46
+	};
+	res.end(JSON.stringify(myObj));
 });
 
 server.listen(3000, '127.0.0.1');
