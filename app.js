bae9fe29a29ebed84348ad3d90fb5f97d6fc0076
BrandoB <realbrandonb602@gmail.com>
2017-09-23 11:16:54 -0700

    lesson 28

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
+app.use('/assets', express.static('assets'));
 
 app.get('/',function(req, res){
 	res.render('index');
