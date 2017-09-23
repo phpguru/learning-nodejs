6c8652822deb9bbbea502613ae011b142cd730f0
phpguru <geoffrey.hoffman@gmail.com>
2017-09-23 10:57:51 -0700

    Using layout and partians with handlebars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+var fs = require('fs');
 var app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -8,22 +9,30 @@ app.get('/', function(req, res){
 	res.render('home');
 });
 
+app.get('/home', function(req, res){
+	res.render('home');
+});
+
 app.get('/styles.css', function(req, res){
 	res.sendFile(__dirname + '/styles.css');
 });
 
 app.get('/contact', function(req, res){
-	res.sendFile(__dirname + '/contact.html');
+	res.render('contact');
 });
 
 app.get('/profile/:name', function(req, res){
 	var data = {
 		age: 46,
 		job: 'guru',
-		slogan: 'I <em>rock</em> the casbah'
+		slogan: 'I <em>rock</em> the casbah',
+		hobbies: [{hobby:'eating'}, {hobby:'fighting'}, {hobby:'fishing'}]
 	};
 	res.render('profile', {person: req.params.name, data: data});
 });
 
+app.use(function (req, res, next) {
+  res.status(404).sendFile(__dirname + '/404.html');
+});
 
 app.listen(3000);
