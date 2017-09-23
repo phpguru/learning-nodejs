9ba9304cdd7a8ec9e88e89c6e97d0cfc9961b002
phpguru <geoffrey.hoffman@gmail.com>
2017-09-23 08:54:17 -0700

    Route parameters

--- app.js
+++ app.js
@@ -10,4 +10,9 @@ app.get('/contact', function(req, res){
 	res.send('This is the contact page');
 });
 
+app.get('/profile/:id', function(req, res){
+	res.send('You requested profile id ' + req.params.id);
+});
+
+
 app.listen(3000);
