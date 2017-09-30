57d6c86ab94529645740956f2b635dc15361d5b1
BrandoB <realbrandonb602@gmail.com>
2017-09-30 09:21:29 -0700

    Lesson 30 adding parser

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
 
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
 app.get('/',function(req, res){
 	res.render('index');
 });
@@ -13,7 +16,12 @@ app.get('/contact',function(req, res){
 	res.render('contact',{qs: req.query});
 });
 
-app.get('/profile/:name', function(req, res){	
+app.post('/contact',urlencodedParser, function(req, res){
+	console.log(req.body);
+	res.render('contact',{qs: req.query});
+});
+
+app.get('/profile/:name', function(req, res){
 	var data = {age: 29, job: 'ninja', hobbies: ['eating', 'fighting', 'fishing']};
 	res.render('profile', {person: req.params.name, data: data});
 });
